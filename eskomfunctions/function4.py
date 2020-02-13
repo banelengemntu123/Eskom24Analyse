36d59610367bed34ddc2408f11bb12beeab9358b
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-14 00:01:16 +0200

    Added dictinary to fucntion4

--- eskomfunctions/function4.py
+++ eskomfunctions/function4.py
@@ -2,16 +2,27 @@ def extract_municipality_hashtags(df):
     """Return a modified pandas dataframe that contains of two new columns about the municipality and the hashtag of the tweets
     Args:
         df(DataFrame): df pandas dataframe
-    
+
     Returns:
         df(DataFrame): DataFrame with new municipality and hashtag columns
 
-    Examples: 
+    Examples:
         >>>> extract_municipality_hashtags(twitter_df.copy())
 
     """
     import numpy as np
     import pandas as pd
+
+    mun_dict = {
+    '@CityofCTAlerts' : 'Cape Town',
+    '@CityPowerJhb' : 'Johannesburg',
+    '@eThekwiniM' : 'eThekwini' ,
+    '@EMMInfo' : 'Ekurhuleni',
+    '@centlecutility' : 'Mangaung',
+    '@NMBmunicipality' : 'Nelson Mandela Bay',
+    '@CityTshwane' : 'Tshwane'
+    }
+
     #create a empty list
     mun_list = []
     for tweet in df['Tweets']:#Iterate through a column and split
