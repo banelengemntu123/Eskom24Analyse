708a1beb0f91658e4f7130acb1a11497283e8d29
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-13 16:44:49 +0200

    fixed indentation

--- eskomfunctions/function3.py
+++ eskomfunctions/function3.py
@@ -2,7 +2,7 @@
 
 def date_parser(dates):
 
-"""
+    """
 
     A function that takes a list of datetime strings and returns only the date in 'yyyy-mm-dd' formates
 
@@ -15,7 +15,8 @@ def date_parser(dates):
     Example:
       >>>>date_parser(dates[:3])
           ['2019-11-29', '2019-11-29', '2019-11-29']
-""""      
+    """    
+
 
     mylist= []
     for date in dates:
