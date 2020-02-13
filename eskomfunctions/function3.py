06eee532830c71d2dacce6c5b2c348896401a03b
Vickyhub-87 <chepape87@gmail.com>
2020-02-13 14:50:25 +0200

    added funtion3 into its own file

--- eskomfunctions/function3.py
+++ eskomfunctions/function3.py
@@ -1,25 +1,25 @@
 #Function3
 
-    def date_parser(dates):
+def date_parser(dates):
 
-    """
-        A function that takes a list of datetime strings and returns only the date in 'yyyy-mm-dd' formates
+"""
+    A function that takes a list of datetime strings and returns only the date in 'yyyy-mm-dd' formates
 
-        Args:
-            dates(list): list of datetime strings
+    Args:
+        dates(list): list of datetime strings
 
-        returns:
-            list: lists of only the date
+    returns:
+        list: lists of only the date
 
-        Example:
-          >>>>date_parser(dates[:3])
+    Example:
+      >>>>date_parser(dates[:3])
           ['2019-11-29', '2019-11-29', '2019-11-29']
     """"      
 
 
-        mylist= []
-        for date in dates:
-            mylist.append(date[:10])
-        return mylist
+    mylist= []
+    for date in dates:
+        mylist.append(date[:10])
+    return mylist
 
 #__________________________________________________________________________________________________________________________________________
