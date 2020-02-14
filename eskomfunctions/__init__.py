93411f9a6bfcf2ae93239dd9373b768aaeb324c2
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-14 12:20:15 +0200

    changefd init

--- eskomfunctions/__init__.py
+++ eskomfunctions/__init__.py
@@ -1,1 +1,2 @@
-from . import function1, function2, function3, function4, function5, function6, function7
+#from . import function1, function2, function3, function4, function5, function6, function7
+from .function1 import dictionary_of_metrics
