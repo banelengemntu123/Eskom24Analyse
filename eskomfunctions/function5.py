b43009d6672bc5465098b0b321c410bbc734090d
Maredipinkydimakatso <maredipinkydimakatso@gmail.com>
2020-02-14 10:57:35 +0200

    Function 5

--- eskomfunctions/function5.py
+++ eskomfunctions/function5.py
@@ -1,5 +1,7 @@
+import pandas as pd 
+
 def number_of_tweets_per_day(df):
-    
+  
     """
     Calculate  the number of tweets that were posted per day
 
@@ -10,13 +12,11 @@ def number_of_tweets_per_day(df):
     Returns:
         df (Dataframe) : dataframe, grouped by day, with the number of tweets for that day.
     Examples:
-        >>> number_of_tweets_per_day(twitter_df.copy())
-        
+        >>> number_of_tweets_per_day(twitter_df.copy())   
     """
 
 
-def number_of_tweets_per_day(df):
-    # your code here
+     # your code here
     df["Date"] = [i.split(' ',1)[0] for i in df["Date"]]
     a = sorted(list(df["Date"].unique()))
     dictp = {}
@@ -24,9 +24,9 @@ def number_of_tweets_per_day(df):
         for x in df[df["Date"] == i]["Date"]:
             if x in dictp.keys():
                 dictp[x] += 1
-            else:
-                dictp[x] = 1 
-    new_df = pd.Dataframe.from_dict(data=dictp,orient="index", columns= ["Tweets"])
+        else:
+            dictp[x] = 1 
+    new_df = pd.Dataframe.from_dict(data=dictp,orient="index", coloumns= ["Tweets"])
     new_df.index.name = "Date"
     
     return new_df
