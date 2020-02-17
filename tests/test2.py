3c3f50610901bf54e7b5e69a71c8c2ef4aebe206
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-17 12:18:39 +0200

    Added comment to test2.py

--- tests/test2.py
+++ tests/test2.py
@@ -2,10 +2,9 @@ from eskomfunctions import function2
 
 def test_five_num_summary:
 
-
-
-        "#Test function to ensure it operates correctly.
-
+    """"
+    Test function to ensure it operates correctly.
+    """
 
     assert five_num_summary([12,125,3,89,36]) == {'min': 3, 'q1': 12.0, 'median': 36.0, 'q3': 89.0, 'max': 125}, 'incorrect'
     assert five_num_summary([100,326,856,369,231,213]) == {'min': 100, 'q1': 217.5, 'median': 278.5, 'q3': 358.25, 'max': 856}, 'incorrect'
