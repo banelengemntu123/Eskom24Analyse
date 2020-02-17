5dd7610a340d0e63e2f866370aadeab38f1c847e
Vickyhub-87 <chepape87@gmail.com>
2020-02-17 12:00:19 +0200

    Updated test3.py

---
+++ tests/test3.py
@@ -0,0 +1,10 @@
+
+def date_parser(dates):
+
+"""
+  Expected Output:
+
+"""
+
+date_parser(dates[:3]) == ['2019-11-29', '2019-11-29', '2019-11-29']
+date_parser(dates[-3:]) == ['2019-11-20', '2019-11-20', '2019-11-20']
