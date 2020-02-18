5b4c9802540c2adcbc640e22ff3c8cab09a6c716
Banele Ngemntu <banelengemntu123@gmail.com>
2020-02-18 13:30:39 +0200

    test file for function 4

--- tests/test4.py
+++ tests/test4.py
@@ -1,3 +1,8 @@
-extract_municipality_hashtags(twitter_df.copy()).loc[4, 'hashtags'] == ['#eskomfreestate', '#mediastatement']
-extract_municipality_hashtags(twitter_df.copy()).loc[5, 'municipality'] == ['Johannesburg']
-extract_municipality_hashtags(twitter_df.copy()).loc[18, 'municipality'] == ['Johannesburg']
+from eskomfunctions import function4
+
+def extract_municipality_hashtags(df):
+
+
+    assert function4.extract_municipality_hashtags(twitter_df.copy()).loc[4, 'hashtags'] == ['#eskomfreestate', '#mediastatement'], 'incorrect'
+    assert function4.extract_municipality_hashtags(twitter_df.copy()).loc[5, 'municipality'] == 'Johannesburg' , 'incorrect'
+    assert function4.extract_municipality_hashtags(twitter_df.copy()).loc[18, 'municipality'] == ['Johannesburg', 'Tshwane'] , 'incorrect'
