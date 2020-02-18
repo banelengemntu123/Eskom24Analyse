c72b20bc83bd00160224ca8874c7b49ce18adc90
Banele Ngemntu <banelengemntu123@gmail.com>
2020-02-18 12:26:56 +0200

    Commit all changes

---
+++ tests/test3.py
@@ -0,0 +1,10 @@
+from eskomfunctions import function3
+
+def test_date_parser(dates):
+
+    """
+    expected outcomes:
+    """
+
+    date_parser(dates[:3]) == ['2019-11-29', '2019-11-29', '2019-11-29']
+    date_parser(dates[-3:]) == ['2019-11-20', '2019-11-20', '2019-11-20']
