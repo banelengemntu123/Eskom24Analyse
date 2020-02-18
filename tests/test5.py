dd0909b1186b0a64e0a37aec856f04fd28d73ec7
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-18 13:25:18 +0200

    Added tests for function 5

--- tests/test5.py
+++ tests/test5.py
@@ -1,1 +1,11 @@
-number_of_tweets_per_day(twitter_df.copy()).loc['2019-11-20', 'Tweets'] == 18
+from eskomfunctions import function5:
+
+def test_number_of_tweets_per_day:
+
+    """"
+    Test function to ensure it operates correctly.
+    """
+
+    assert number_of_tweets_per_day(twitter_df.copy()).loc['2019-11-20', 'Tweets'] == 18, 'incorrect'
+    assert number_of_tweets_per_day(twitter_df.copy()).loc['2019-11-22', 'Tweets'] == 25, 'incorrect'
+    assert number_of_tweets_per_day(twitter_df.copy()).loc['2019-11-28', 'Tweets'] == 32, 'incorrect'
