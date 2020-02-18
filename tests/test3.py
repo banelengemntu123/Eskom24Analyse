df5a57d98a4f000a3cf5708c043ead0dd01c56d3
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-18 13:48:23 +0200

    Called file name before calling function in test files

--- tests/test3.py
+++ tests/test3.py
@@ -6,6 +6,6 @@ def test_date_parser(dates):
     Test function to ensure it operates correctly.
     """
 
-    assert date_parser(dates[:3]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
-    assert date_parser(dates[-3:]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
-    assert date_parser(dates[3:6]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
+    assert function3.date_parser(dates[:3]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
+    assert function3.date_parser(dates[-3:]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
+    assert function3.date_parser(dates[3:6]) == ['2019-11-29', '2019-11-29', '2019-11-29'], 'incorrect'
--- tests/test7.py
+++ tests/test7.py
@@ -6,6 +6,6 @@ def test_stop_words_remover:
     Test function to ensure it operates correctly.
     """
 
-    assert stop_words_remover(twitter_df.copy()).loc[0, "Without Stop Words"] == ['@bongadlulane', 'send', 'email', 'mediadesk@eskom.co.za'], incorrect
-    assert stop_words_remover(twitter_df.copy()).loc[100, "Without Stop Words"] == ['#eskomnorthwest', '#mediastatement', ':', 'notice', 'supply', 'interruption', 'lichtenburg', 'area', 'https://t.co/7hfwvxllit'], incorrect
-    assert stop_words_remover(twitter_df.copy()).loc[4, 'Without Stop Words'] == ['#eskomfreestate', '#mediastatement', ':', 'eskom', 'suspends', 'planned', 'electricity', 'supply', 'interruptions', 'mangaung', 'metropolitan…', 'https://t.co/mtbo6gjcte'], incorrect
+    assert function7.stop_words_remover(twitter_df.copy()).loc[0, "Without Stop Words"] == ['@bongadlulane', 'send', 'email', 'mediadesk@eskom.co.za'], incorrect
+    assert function7.stop_words_remover(twitter_df.copy()).loc[100, "Without Stop Words"] == ['#eskomnorthwest', '#mediastatement', ':', 'notice', 'supply', 'interruption', 'lichtenburg', 'area', 'https://t.co/7hfwvxllit'], incorrect
+    assert function7.stop_words_remover(twitter_df.copy()).loc[4, 'Without Stop Words'] == ['#eskomfreestate', '#mediastatement', ':', 'eskom', 'suspends', 'planned', 'electricity', 'supply', 'interruptions', 'mangaung', 'metropolitan…', 'https://t.co/mtbo6gjcte'], incorrect
