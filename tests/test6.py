a5418661ec335f2404ac9645d46039ffd531417d
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-18 13:43:11 +0200

    Added example test to test function6

--- tests/test6.py
+++ tests/test6.py
@@ -1,8 +1,11 @@
 from eskomfunctions import function6
 
 def word_splitter:
-    
+
     """"
     Test function to ensure it operates correctly.
     """
     assert word_splitter(df) == , 'incorrect'
+
+    example test:
+    assert word_splitter(twitter_df.copy()).loc[0, 'Split Tweets'] == ['@bongadlulane', 'please', 'send', 'an', 'email', 'to', 'mediadesk@eskom.co.za'], 'incorrect'
