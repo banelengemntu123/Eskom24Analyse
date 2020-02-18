4ac84f0a8bc2945e1938c3a1099cd6ee5d3ed8d1
mrmamadi <60425737+mrmamadi@users.noreply.github.com>
2020-02-18 12:07:08 +0200

    Added tests to tests7

--- tests/test7.py
+++ tests/test7.py
@@ -2,10 +2,9 @@ from eskomfunctions import function7
 
 def test_stop_words_remover:
 
-
-    #test fucntions
-
-
+    """"
+    Test function to ensure it operates correctly.
+    """
 
     assert stop_words_remover(twitter_df.copy()).loc[0, "Without Stop Words"] == ['@bongadlulane', 'send', 'email', 'mediadesk@eskom.co.za'], incorrect
     assert stop_words_remover(twitter_df.copy()).loc[100, "Without Stop Words"] == ['#eskomnorthwest', '#mediastatement', ':', 'notice', 'supply', 'interruption', 'lichtenburg', 'area', 'https://t.co/7hfwvxllit'], incorrect
